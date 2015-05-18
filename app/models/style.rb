b0fcd99017b60bb9bb51aec3eca01c915ccf39b4
minsikzzang <minsikzzang@gmail.com>
2015-05-18 23:32:46 +0100

    Add site style json

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,12 @@
 class Style < ActiveRecord::Base
   belongs_to :site
+
+  def to_h
+    self.attributes.merge({
+      logo: {
+        small_url: logo.small.url,
+        logo: logo.url
+      }
+    }).except!("logo")
+  end
 end
