7a99261912a9d4fbd4ac36276ef8a2d8af1c4012
Min Kim <minsikzzang@gmail.com>
2015-05-19 09:43:45 +0100

    Remove aws key secret

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     'AKIAJOLRQ5O3RQ3PCCPQ',
-    aws_secret_access_key: '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',
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region:                'eu-west-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
