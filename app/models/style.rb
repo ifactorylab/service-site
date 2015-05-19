51806e20d0fbd99945dc9b2917229216c6aa4d34
minsikzzang <minsikzzang@gmail.com>
2015-05-19 21:28:42 +0100

    Remove aws credential

--- app/models/style.rb
+++ app/models/style.rb
@@ -2,6 +2,13 @@ class Style < ActiveRecord::Base
   belongs_to :site
   mount_uploader :logo, LogoUploader
 
+  before_save do
+    self.header_background_color = "#9edccd" unless self.header_background_color
+    self.header_title_color = "#ffffff" unless self.header_title_color
+    self.footer_background_color = "#9edccd" unless self.footer_background_color
+    self.footer_title_color = "#ffffff" unless self.footer_title_color
+  end
+
   def to_h
     self.attributes.merge({
       logo: {
