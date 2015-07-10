1821fecf82fdc5006a5e13ac27fe736ce62b858e
minsikzzang <minsikzzang@gmail.com>
2015-07-10 21:32:27 +0100

    Change CarrierWave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,13 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    #aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    #aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    aws_access_key_id:     'rte',
-    aws_secret_access_key: 'te',
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region:                'eu-west-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
+
   config.storage        = :fog
   config.fog_directory  = 'service-content-development'
   config.fog_public     = false
