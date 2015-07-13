2eb38bf4dce40fdb3dfb551f9ff4dd8b29718104
minsikzzang <minsikzzang@gmail.com>
2015-07-13 20:59:44 +0100

    Change back to production stuff

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   # config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',
-    # aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
-    # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    aws_access_key_id:     "test1234",
-    aws_secret_access_key: "test1234",
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+    #aws_access_key_id:     "test1234",
+    #aws_secret_access_key: "test1234",
     region:                'eu-west-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
