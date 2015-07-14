c49bdc79a31ae1759525a7a550b7523d83477da4
minsikzzang <minsikzzang@gmail.com>
2015-07-14 22:19:41 +0100

    Change url for booking request link'

--- app/mailers/partner_mailer.rb
+++ app/mailers/partner_mailer.rb
@@ -3,6 +3,7 @@ class PartnerMailer < ApplicationMailer
 
   def booking_request_email(booking)
     @booking = booking
+    @url = "web-admin.herokuapp.com"
     mail(to: @booking.email, subject: "You've got booking request")
   end
 
