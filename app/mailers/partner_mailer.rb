80f039dab2e6ab03a2e50a28789af7da134ce6dd
minsikzzang <minsikzzang@gmail.com>
2015-08-06 22:44:21 +0100

    Fix people unit properly

--- app/mailers/partner_mailer.rb
+++ app/mailers/partner_mailer.rb
@@ -4,7 +4,13 @@ class PartnerMailer < ApplicationMailer
   def booking_request_email(booking)
     @booking = booking
     @url = "web-admin.herokuapp.com"
+    @people_unit = unit_for_people(booking)
     mail(to: @booking.site.business.email, subject: "You have a booking request")
   end
 
+  private
+
+  def unit_for_people(booking)
+    @booking.number_of_person > 1 ? "people" : "person"
+  end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,15 +4,21 @@ class UserMailer < ApplicationMailer
   def booking_confirm_email(booking)
     @booking = booking
     @email = Email.find_booking_confirm(booking.site)
+    @people_unit = unit_for_people(booking)
     mail(to: @booking.email, from: business_email(booking), subject: "Your booking has been confirmed with #{@booking.site.business.name}")
   end
 
   def booking_reject_email(booking)
     @booking = booking
+    @people_unit = unit_for_people(booking)
     mail(to: @booking.email, from: business_email(booking), subject: "Your booking has been rejected")
   end
 
   private
+
+  def unit_for_people(booking)
+    @booking.number_of_person > 1 ? "people" : "person"
+  end
 
   def business_email(booking)
     booking.site.business.email
