82141fcf6b96e609e785f4db49d966c091031859
minsikzzang <minsikzzang@gmail.com>
2015-08-06 21:56:27 +0100

    Update email from for UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,18 @@ class UserMailer < ApplicationMailer
   def booking_confirm_email(booking)
     @booking = booking
     @email = Email.find_booking_confirm(booking.site)
-    mail(to: @booking.email, subject: "Your booking has been confirmed with #{@booking.site.business.name}")
+    mail(to: @booking.email, from: business_email(booking), subject: "Your booking has been confirmed with #{@booking.site.business.name}")
   end
 
   def booking_reject_email(booking)
     @booking = booking
-    mail(to: @booking.email, subject: "Your booking has been rejected")
+    mail(to: @booking.email, from: business_email(booking), subject: "Your booking has been rejected")
+  end
+
+  private
+
+  def business_email(booking)
+    booking.site.business.email
   end
 
 end
