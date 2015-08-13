b1ef0583ec60e46443a872f10f7ec51871b92258
minsikzzang <minsikzzang@gmail.com>
2015-08-13 22:19:54 +0100

    Update app_url

--- app/models/site.rb
+++ app/models/site.rb
@@ -13,7 +13,7 @@ class Site < ActiveRecord::Base
   end
 
   before_save do
-    self.app_url = "http://#{APP_SERVER}/?#{self.id}"
+    self.app_url = "http://#{APP_SERVER}/#!#{self.id}"
     if self.domain
       # self.domain.gsub!("http://", "")
       self.domain = self.domain[0...-1] if self.domain[-1, 1] == "/"
@@ -25,7 +25,7 @@ class Site < ActiveRecord::Base
   after_save :update_app_url
 
   def update_app_url
-    self.update_column(:app_url, "http://#{APP_SERVER}/?#{self.id}")
+    self.update_column(:app_url, "http://#{APP_SERVER}/#!#{self.id}")
     # self.update_column(:domain, "http://web-app-angular.herokuapp.com/#/#{self.id}")
 
     Style.create!(:title => self.name, :site_id => self.id) unless self.style
